e6f7f820d2f2771d6e36a21039f61c6350ab4c0f
Gareth Powell <gareth@ziniki.org>
2018-04-15 13:25:59 +0100

    Started put implementation

--- src/main/javascript/runtime/croset.js
+++ src/main/javascript/runtime/croset.js
@@ -2,9 +2,11 @@
 
 _Croset = function(service, id, sortBy) {
 	"use strict";
+	this.service = service;
 	this.id = null;
 	this.cliId = 1;
 	this.elements = [];
+	this.putState = { start: [] };
 	
 	var self = this;
 	if (id == null) {
@@ -27,7 +29,15 @@ _Croset.prototype.at = function(pos) {
 
 _Croset.prototype.prepend = function(item) {
 	"use strict";
-	this.elements.splice(0, 0, this.makeElement(item));
+	var elt = this.makeElement(item);
+	this.elements.splice(0, 0, elt);
+	this.putState.start.splice(0, 0, elt.clientId);
+	if (this.id) {
+		var self = this;
+		this.service.put(this.putState, function(reply) {
+			self.applyPutUpdate(reply);
+		});
+	}
 }
 
 _Croset.prototype.append = function(item) {
@@ -36,6 +46,7 @@ _Croset.prototype.append = function(item) {
 }
 
 _Croset.prototype.makeElement = function(item) {
+	"use strict";
 	// TODO: handle natural crokey more directly (don't need clientId)
 	var c = this.cliId++;
 	// TODO: make a server request IFF we have an ID for the collection
@@ -45,6 +56,23 @@ _Croset.prototype.makeElement = function(item) {
 		serverId: null,
 		item: item
 	};
+}
+
+_Croset.prototype.applyPutUpdate = function(reply) {
+	"use strict";
+	var cids = reply.cids;
+	for (var ci=0;ci<cids.length;ci++) {
+		var cid = cids[ci].c;
+		var idx = this.putState.start.indexOf(cid);
+		if (idx > -1) {
+			for (var j=0;j<this.elements.length;j++)
+				if (this.elements[j].clientId == cid) {
+					this.elements[j].serverId = cids[ci].s; 
+					break;
+				}
+			this.putState.start.splice(idx, 1);
+		}
+	}
 }
 
 Croset = function() { }
