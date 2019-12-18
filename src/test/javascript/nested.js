9654327b0760bb5217c99f34e5dd26eaaa4f06b7
Gareth Powell <gareth@ziniki.org>
2019-12-18 07:42:22 -0500

    Deleted old crud

--- src/test/javascript/nested.js
+++
@@ -1,8 +0,0 @@
-var f = function(x) {
-  var g = function(y) {
-    return x*y;
-  }
-  return g(2);
-}
-
-console.log(f(3));
--- src/test/scripts/testfib.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var vm = require('vm');
-var FLEval = vm.runInThisContext(fs.readFileSync('../javascript/flenv.js', 'utf8'));
-var fib = vm.runInThisContext(fs.readFileSync('../javascript/fib.js', 'utf8'));
-
-for (var i=0;i<10;i++)
-	console.log(FLEval.head(fib(i)));
