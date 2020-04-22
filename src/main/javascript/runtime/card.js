6b49f47dbac51f4384fa5dbf2a35ad0a2d6d3e9c
Gareth Powell <gareth@ziniki.org>
2020-04-22 12:45:09 -0400

    handling content nodes

--- src/main/javascript/runtime/card.js
+++ src/main/javascript/runtime/card.js
@@ -2,18 +2,31 @@ const FLCard = function(cx) {
     this._currentDiv = null;
 }
 
-FLCard.prototype.renderInto = function(cx, div) {
+FLCard.prototype.renderInto = function(_cxt, div) {
     div.innerHTML = '';
     this._currentDiv = div;
     if (this._template) {
         var t = document.getElementById(this._template);
         if (t != null) {
             var clone = t.content.cloneNode(true);
-            // TODO: we need to set things here based on card state
+            this._updateDisplay(_cxt, clone);
             div.appendChild(clone);
         }
     }
 }
+
+FLCard.prototype._updateContent = function(_cxt, root, field, value) {
+    // it should not be necesary to evaluate anything
+    // we should not store partially evaluated items
+    if (!value)
+        value = '';
+    const nodes = root.querySelectorAll("[data-flas-content='" + field + "']");
+    for (var i=0;i<nodes.length;i++) {
+        nodes[i].innerHTML = '';
+        nodes[i].appendChild(document.createTextNode(value));
+    }
+}
+
 //--EXPORT
 /* istanbul ignore else */ 
 if (typeof(module) !== 'undefined')
