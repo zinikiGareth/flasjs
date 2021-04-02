e3446db35d48a4f8475fe0ae46f7bc736e19ae86
Gareth Powell <gareth@ziniki.org>
2021-04-02 11:39:58 +0100

    Hacking in invocation of lc.load

--- src/main/javascript/container/appl.js
+++ src/main/javascript/container/appl.js
@@ -8,6 +8,11 @@ const Application = function(_cxt) {
 Application.prototype.gotoRoute = function(_cxt, r) {
 	var card = new this.mainCard(_cxt);
 
+	var ctr = _cxt.findContractOnCard(card, "Lifecycle");
+	if (ctr && ctr.load) {
+		_cxt.env.queueMessages(_cxt, ctr.load(_cxt, "loaded"));
+	}
+
 	// the main card is always called "main"
 	this.cards.main = card;
 }
