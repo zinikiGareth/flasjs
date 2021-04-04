e9ceea69a33a59410b28ca1e5793d79aab0c25bd
Gareth Powell <gareth@ziniki.org>
2021-04-04 16:35:41 +0100

    Handled nesting routing up to a point

--- src/main/javascript/container/appl.js
+++ src/main/javascript/container/appl.js
@@ -4,18 +4,70 @@ const FLError = require('../runtime/error');
 const Application = function(_cxt, topdiv) {
 	this.topdiv = topdiv;
 	this.cards = {};
+	this.currentRoute = null; // TODO: this should not just be a list of strings, but of UNDO actions
+}
+
+Application.prototype.baseUri = function(_cxt) {
+	return ''; // could be something like https://foo.com/app; set in the actual application object
 }
 
 Application.prototype.gotoRoute = function(_cxt, r) {
 	var routing = this._routing();
 	console.log("routing ", routing);
-	var card = new this.mainCard(_cxt);
-	card._renderInto(_cxt, this.topdiv);
+	if (this.currentRoute == null) {
+		this.currentRoute = [];
+		this._createCards(_cxt, [{ name: 'main', card: this.mainCard }]);
+		this._enterRoute(_cxt, routing.enter);
+		this.cards.main._renderInto(_cxt, this.topdiv);
+	}
+	var path = this.parseRoute(_cxt, r);
+	console.log("have", this.currentRoute, "want", path);
 
-	var ctr = _cxt.findContractOnCard(card, "Lifecycle");
-	if (ctr) {
-		for (var i=0;i<routing.enter.length;i++) {
-			var a = routing.enter[i];
+	// remove and ignore any common elements
+	// move "up" if current has anything left
+	// move "down" the new path
+	this.moveDown(_cxt, routing, path);
+}
+
+Application.prototype.parseRoute = function(_cxt, r) {
+	if (r instanceof Location) {
+		r = r.href;
+	}
+	var url = r.replace(this.baseUri(), '').replace(/^#*/, '').replace(/^\/*/, '');
+	var parts = url.split("/").filter(x => !!x);
+	return parts;
+}
+
+Application.prototype.moveDown = function(_cxt, table, path) {
+	if (path.length == 0)
+		return;
+
+	var first = path[0];
+	for (var i=0;i<table.routes.length;i++) {
+		var rr = table.routes[i];
+		if (rr.path == first) {
+			this.currentRoute.push({ action: rr });
+			this._createCards(_cxt, rr.cards);
+			this._enterRoute(_cxt, rr.enter);
+			return; // don't consider any other matches or fall through to parameter logic
+		}
+	}
+	// TODO: maybe it is a parameter
+}
+
+Application.prototype._createCards = function(_cxt, cards) {
+	for (var i=0;i<cards.length;i++) {
+		var ci = cards[i];
+		this.cards[ci.name] = new ci.card(_cxt);
+	}
+}
+
+Application.prototype._enterRoute = function(_cxt, enter) {
+	for (var i=0;i<enter.length;i++) {
+		var a = enter[i];
+		var card = this.cards[a.card];
+		var ctr = _cxt.findContractOnCard(card, "Lifecycle");
+		if (ctr) {
 			var m = a.action;
 			if (ctr[m]) {
 				var msgs;
@@ -29,9 +81,6 @@ Application.prototype.gotoRoute = function(_cxt, r) {
 			}
 		}
 	}
-
-	// the main card is always called "main"
-	this.cards.main = card;
 }
 
 Application.prototype._currentRenderTree = function() {
